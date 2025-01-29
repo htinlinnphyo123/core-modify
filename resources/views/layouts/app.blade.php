2bba42ec36e0df7cb2d88d7e4c534aec09f3130e
Nay Ba La <naybalaismylife.nbl@gmail.com>
2025-01-29 16:56:10 +0630

    delete : extra migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,20 +7,11 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- csrf token need for using ajax post method --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Buddhist News</title>
+    <title>Core Modify</title>
     <link rel="icon" type="image/jpg"
-        href="{{ asset('https://buddha.sgp1.digitaloceanspaces.com/logo/2024-08-30%2015.45.06.jpg') }}">
+        href="{{ asset('https://avatars.githubusercontent.com/u/80409797?s=200&v=4') }}">
     @vite(['resources/css/app.css', 'resources/css/custom.css', 'resources/js/app.js'])
-    <script src="https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.page.js" defer></script>
-    <script>
-        window.OneSignalDeferred = window.OneSignalDeferred || [];
-        OneSignalDeferred.push(async function(OneSignal) {
-            await OneSignal.init({
-                appId: "{{ env('ONE_SIGNAL_APP_ID') }}",
-                allowLocalhostAsSecureOrigin: true,
-            });
-        });
-    </script>
+   
 </head>
 
 <body class="dark:bg-black">
