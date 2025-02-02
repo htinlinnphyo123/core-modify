515e7b4798cd7bc8eb014e86456361d82db37f13
Htin Linn Phyo <htinlinnphyo.npt@gmail.com>
2025-02-02 19:27:08 +0630

    Remove Blog Feature

--- lang/en/blog.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'name' => 'Name',
-    'name_validation' => "Name is required",
-    'blog_created' => "???? was successfully created",
-    'blog_updated' => "???? was successfully updated",
-    'blog_deleted' => "???? was successfully deleted",
-];
